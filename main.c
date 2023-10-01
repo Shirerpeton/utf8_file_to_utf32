4ede060ce1f4aa2e9db9265a9dd0c015f87ff792
Shirerpeton <hyloxalusfallax@gmail.com>
2023-10-01 15:38:14 +0400

    init commit

---
+++ main.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <wchar.h>
+#include <locale.h>
+#include <stdlib.h>
+
+int main() {
+    setlocale(LC_ALL, "en_US.UTF-8");  // set the locale
+
+    FILE *file = fopen("test2.txt", "r");
+
+    if(!file) {
+        puts("can't open file");
+    }
+    const unsigned int BUF_SIZE = 8; 
+    unsigned char *buf = malloc(BUF_SIZE + 1 + 3); // 1 for null teminator + 3 for max overflow from previous chunk
+    const unsigned int DEST_SIZE = 65535;
+    wchar_t *dest = malloc(DEST_SIZE * sizeof(wchar_t));
+    unsigned int dest_length = 0;
+    char overflow_buf[4];
+    unsigned int overflow_buf_len = 0;
+    unsigned char *buf_start = buf;
+    while(1) {
+        size_t bytes_read = fread(buf_start, sizeof(char), BUF_SIZE, file);
+        if(ferror(file) || bytes_read <= 0) {
+            puts("error reading a file");
+            return -1;
+        }
+
+        overflow_buf_len = 0;
+        while(1) {
+            unsigned char prefix = (buf_start[bytes_read - 1 - overflow_buf_len]) & 0b11000000;
+            if(prefix == 0b10000000) {
+                overflow_buf[overflow_buf_len] = buf_start[bytes_read - 1 - overflow_buf_len];
+                overflow_buf_len++;
+            } else if(prefix == 0b11000000) {
+                unsigned int trailing_bytes = 0;
+                for(unsigned int i = 6; i > 3; i--) {
+                    if(buf_start[bytes_read - 1 - overflow_buf_len] & (1 << i)) {
+                        trailing_bytes++;
+                    } else {
+                        break;
+                    }
+                }
+                if(trailing_bytes > overflow_buf_len) {
+                    overflow_buf[overflow_buf_len] = buf_start[bytes_read - 1 - overflow_buf_len];
+                    overflow_buf_len++;
+                    buf_start[bytes_read - overflow_buf_len] = '\0';
+                } else {
+                    overflow_buf_len = 0;
+                    buf_start[bytes_read] = '\0';
+                }
+                break;
+            } else {
+                buf_start[bytes_read - overflow_buf_len] = '\0';
+                break;
+            }
+        }
+        unsigned char *buf_temp = buf;
+        size_t wchars_written = mbsrtowcs(dest + dest_length, (const char **)&buf, DEST_SIZE, NULL);
+        buf = buf_temp;
+        if(wchars_written == -1) {
+            return -1;
+        }
+        dest_length += wchars_written;
+        for(int i = overflow_buf_len - 1; i >= 0; i--) {
+            buf[overflow_buf_len - 1 - i] = overflow_buf[i];
+        }
+        buf_start = buf + overflow_buf_len;
+        overflow_buf_len = 0;
+
+        if(feof(file)) {
+            break;
+        }
+    }
+
+    fputws(L"file content:\n", stdout);
+    fputws(dest, stdout);
+    
+    free(buf);
+    free(dest);
+    fclose(file);
+
+    return 0;
+}
+
+
